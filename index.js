1ac8f271b2d71de5101c48129a7e9a8fab26f3bf
Shivam Sourav Jha <60891544+shivamsouravjha@users.noreply.github.com>
2020-10-01 17:20:41 +0530

    Revert "ADDED RESPONSIVE UI"

--- index.js
+++
@@ -1,7 +0,0 @@
-console.log("Connected!");
-
-const elements = document.querySelectorAll(".element");
-
-elements.forEach(element => element.addEventListener("click", () => {
-  element.classList.toggle("styleClass");
-}));
